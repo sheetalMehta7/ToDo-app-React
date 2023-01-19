4803684b3eb8a7a7da3783c4607da66a8ae89575
Sheetal Mehta <sheetalmehta137@gmail.com>
2023-01-19 11:43:31 +0530

    first commit

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import styles from "./CourseInput.module.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+    setEnteredValue("");
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div
+        className={`${styles["form-control"]} ${
+          !isValid ? styles["invalid"] : ""
+        }`}
+      >
+        <label>Course Goal</label>
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          value={enteredValue}
+        />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
+
+/* 
+Dynamically Adding CSS Inline Styles Classes:
+<label style={{color: isValid? "red": "black"}}>Course Goal</label>
+
+Dynamically Adding CSS Classes:
+<div className={`form-control ${!isValid ? "invalid" : ""}`}>
+
+*/
